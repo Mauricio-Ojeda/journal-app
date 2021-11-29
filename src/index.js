586f1e94a4fd4b6d3884252d665e8e9e0d214ff4
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-11-29 20:06:25 -0300

    start project || add sass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import JournalApp from './JournalApp';
+
 import reportWebVitals from './reportWebVitals';
+
+import './styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <JournalApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
