1c682834087976a33056b4733d13694dc90f0266
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-12-02 17:53:51 -0300

    Login styles

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,4 +1,5 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
 
 const LoginScreen = () => {
     return (
@@ -30,38 +31,61 @@ const LoginScreen = () => {
                   
             }} 
         >{ ( { errors } ) => (
-            <div >
+            <>
+                <h3 className="auth__title">Login</h3>
                 <Form >
                         <div >
                             <label htmlFor="email" className="form-label">Email address</label>
                             <Field 
                                 type="email" 
-                                className="form-control" 
+                                className="auth__input" 
                                 id="email" 
                                 name="email"
                                 aria-describedby="emailHelp"
+                                autoComplete="off"
 
                             />
-                            <ErrorMessage name="email" component={ () =>( <p className="bg-danger text-white mt-1 p-2">{ errors.email }</p> )} />
+                            <ErrorMessage name="email" component={ () =>( <p className="auth__alert-error">{ errors.email }</p> )} />
                             
                         </div>
                         <div >
                             <label htmlFor="password" className="form-label">Password</label>
                             <Field 
                                 type="password" 
-                                className="form-control" 
+                                className="auth__input" 
                                 id="password"
                                 name="password"
                                 autoComplete="off"
 
                             />
-                            <ErrorMessage name="password" component={ () =>( <p>{ errors.password }</p> )} />
+                            <ErrorMessage name="password" component={ () =>( <p className="auth__alert-error">{ errors.password }</p> )} />
                         </div>
                         <div >
-                            <button type="submit" >Login</button>
+                            <button type="submit" className="btn btn-primary btn-block"  >Login</button>
                         </div>
+                       
+                        <div className="auth__social-networks">
+                            <p>
+                                Login with social networks
+                            </p>
+                            <div 
+                                className="google-btn"
+                            >
+                                <div className="google-icon-wrapper">
+                                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                                </div>
+                                <p className="btn-text">
+                                    <b>Sign in with google</b>
+                                </p>
+                            </div>
+                        </div>
+                                <Link 
+                                    to="/auth/register"
+                                >                                    
+                                    Create new account
+                                </Link>
                 </Form>
-            </div> 
+            </> 
         )}
         </Formik>          
     )
