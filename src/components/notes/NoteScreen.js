f0819c42b9e92d1bd951f916ca6337e4b0de935e
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-12-05 09:24:20 -0300

    home screen styles

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,29 @@ const NoteScreen = () => {
         <div className="notes__main-content">
 
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input 
+                    type="text" 
+                    placeholder="Write your awesome title" 
+                    className="notes__title-input"
+                    name="" 
+                    id="" 
+
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                ></textarea>
+                <div className="notes__image">
+                    <img 
+                        src="https://us.123rf.com/450wm/noravector/noravector1901/noravector190100277/115431176-you-re-awesome-vector-illustrated-comic-book-style-phrase-on-abstract-background-.jpg?ver=6" 
+                        alt="awesome" 
+
+                    />
+                </div>
+
+            </div>
             
         </div>
     )
