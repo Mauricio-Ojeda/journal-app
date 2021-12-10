79ce08c86849d7d457149b58a70440a2cdeac23e
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-12-10 15:00:57 -0300

    firts dispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,99 +1,114 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
 
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
 
 const LoginScreen = () => {
-    return (
-        <Formik
-            initialValues={{
-                    email: '',
-                    password: '',
-                    
-                }}
-            validate = {( values ) =>{
-                let error = {};
-                const { email, password } = values;
-                if( !email ){
-                    error.email = 'Please enter your Email'
-                };
+  const [loginValues, setLoginValues] = useState({});
 
-                if( !password || password.trim() === ''){
-                    error.password = 'Please enter your password'
-                } else if ( password.length < 4 ){
-                    error.password = 'Minimum length is 4 characters'
-                } 
+  const dispatch = useDispatch();
 
-                return error;
-            }}
+  
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validate={(values) => {
+        let error = {};
+        const { email, password } = values;
+        if (!email) {
+          error.email = "Please enter your Email";
+        }
 
-            onSubmit ={ ( values, { resetForm } ) => {
-                    resetForm();
-                    
-                  
-            }} 
-        >{ ( { errors } ) => (
+        if (!password || password.trim() === "") {
+          error.password = "Please enter your password";
+        } else if (password.length < 4) {
+          error.password = "Minimum length is 4 characters";
+        }
 
-            <>  
-                
-                <h3 className="auth__title">Login</h3>
+        return error;
+      }}
+      onSubmit={(values, { resetForm }) => {
+        setLoginValues(values);
+        dispatch( login( 123456, 'Mauricio') );
+        resetForm();
+      }}
+    >
+      {({ errors }) => (
+        <>
+          <h3 className="auth__title">Login</h3>
 
-                <Form >
-                        <div >
-                            <label htmlFor="email" className="form-label">Email address</label>
-                            <Field 
-                                type="email" 
-                                className="auth__input" 
-                                id="email" 
-                                name="email"
-                                aria-describedby="emailHelp"
-                                autoComplete="off"
+          <Form>
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <Field
+                type="email"
+                className="auth__input"
+                id="email"
+                name="email"
+                aria-describedby="emailHelp"
+                autoComplete="off"
+              />
+              <ErrorMessage
+                name="email"
+                component={() => (
+                  <p className="auth__alert-error">{errors.email}</p>
+                )}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <Field
+                type="password"
+                className="auth__input"
+                id="password"
+                name="password"
+                autoComplete="off"
+              />
+              <ErrorMessage
+                name="password"
+                component={() => (
+                  <p className="auth__alert-error">{errors.password}</p>
+                )}
+              />
+            </div>
+            <div>
+              <button type="submit" className="btn btn-primary btn-block">
+                Login
+              </button>
+            </div>
 
-                            />
-                            <ErrorMessage name="email" component={ () =>( <p className="auth__alert-error">{ errors.email }</p> )} />
-                            
-                        </div>
-                        <div >
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <Field 
-                                type="password" 
-                                className="auth__input" 
-                                id="password"
-                                name="password"
-                                autoComplete="off"
+            <div className="auth__social-networks">
+              <p>Login with social networks</p>
+              <div className="google-btn">
+                <div className="google-icon-wrapper">
+                  <img
+                    className="google-icon"
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                    alt="google button"
+                  />
+                </div>
+                <p className="btn-text">
+                  <b>Sign in with google</b>
+                </p>
+              </div>
+            </div>
+            <Link to="/auth/register" className="link">
+              Create new account
+            </Link>
+          </Form>
+        </>
+      )}
+    </Formik>
+  );
+};
 
-                            />
-                            <ErrorMessage name="password" component={ () =>( <p className="auth__alert-error">{ errors.password }</p> )} />
-                        </div>
-                        <div >
-                            <button type="submit" className="btn btn-primary btn-block"  >Login</button>
-                        </div>
-                       
-                        <div className="auth__social-networks">
-                            <p>
-                                Login with social networks
-                            </p>
-                            <div 
-                                className="google-btn"
-                            >
-                                <div className="google-icon-wrapper">
-                                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
-                                </div>
-                                <p className="btn-text">
-                                    <b>Sign in with google</b>
-                                </p>
-                            </div>
-                        </div>
-                                <Link 
-                                    to="/auth/register"
-                                    className="link"
-                                >                                    
-                                    Create new account
-                                </Link>
-                </Form>
-            </> 
-        )}
-        </Formik>          
-    )
-}
-
-export default LoginScreen
+export default LoginScreen;
